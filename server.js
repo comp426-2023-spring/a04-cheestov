8a474e99b76f7304e059e48bc21e8b7577884cc4
Cat Tovey <cheestov@email.unc.edu>
2023-05-07 08:53:44 -0400

    first couple of endpoint definitions

--- server.js
+++ server.js
@@ -10,3 +10,12 @@ api.use(express.json());
 const args = minimist(process,argv.slice(2));
 const port = args.port || 5000;
 
+api.get("/app/", (req, res, next) => {
+    res.status(200).json({"message":"200 OK"});
+});
+
+api.get("app/rps", (req, res, next) => {
+    const response = rps();
+    res.status(200).json({player});
+});
+
